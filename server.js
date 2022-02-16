5305f04c62f83f7546fc0421b7dc4dcb35448d08
Hamzah Al-Hagi <ha.773774142@gmail.com>
2022-02-16 23:12:52 +0300

    Golable header and footer and include the style

--- server.js
+++ server.js
@@ -1,34 +1,24 @@
 const express = require('express');
 const app = express();
+
+
+app.use(express.static('public'))
+app.use('/css',express.static(__dirname + 'public/css'))
+app.use('/js',express.static(__dirname + 'public/js'))
+app.use('/images',express.static(__dirname + 'public/images'))
+// app.get('/',function(req,res){
+//  res.sendFile(__dirname + '/index.html');
+
+// });
+
+// view
+app.set('views','./views');
 app.set('view engine','ejs');
-app.get('/',function(req,res){
-    res.sendFile(__dirname + '/index.html');
+///home
+//home
+app.get('',(req,res)=>{
+    res.render('home');
 });
-app.get('/headfooter',(req,res)=>{
-    res.render('headfooter',{titl_name:'Hamzah'})
-});
-var list =[
-    {
-        name:'Hamzah',
-        age:'27'
-    },
-    {
-        name:'Ali',
-        age:'25'
-    },
-    {
-        name:'Khaild',
-        age:'30'
-    },
-    {
-        name:'Wedad',
-        age:'23'
-    },
-    {
-        name:'Hamzah',
-        age:'27'
-    },
-]
 
 // app.get('/about',function);
 app.listen(3000,function(req,res){
